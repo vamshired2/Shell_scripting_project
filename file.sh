5f05152c44352ecf0374116a94654d0e3c27d357
root <root@ip-10-0-1-221.ap-south-1.compute.internal>
2025-02-25 09:36:15 +0000

    First version

---
+++ file.sh
@@ -0,0 +1,4 @@
+clone and fork:
+====================================
+clone: Creates a local copy of remote repository on your machine
+fork: Crates a separate copy of the repository under github account
