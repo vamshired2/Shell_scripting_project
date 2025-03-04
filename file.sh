3ba7baf5cd91d2d04eb02ab506ba021110b46d48
vamshired2 <120205094+vamshired2@users.noreply.github.com>
2025-03-04 04:49:44 +0000

    Pending changes exported from your codespace

--- file.sh
+++ file.sh
@@ -1,4 +1,2 @@
-clone and fork:
-====================================
-clone: Creates a local copy of remote repository on your machine
-fork: Crates a separate copy of the repository under github account
+#!/bin/bash
+echo "This is vamshi"
